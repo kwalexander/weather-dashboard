b2688f4fcc06b433e077308be632be454917337e
Kaspar Alexander <kaspar.alexander@gmail.com>
2022-10-18 20:30:53 -0400

    JS function approach

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// define variables
+
+// listen for user city search
+
+// convert search to lat/long coordinates
+
+// query weather API with searched city
+
+// display city current weather
+
+// display city 5 day forecast
+
+// save searched city to local storage and populate recent searches
