ab1a7e2015cb9d87979c134aab9291607ecf0e2b
Kaspar Alexander <kaspar.alexander@gmail.com>
2022-10-19 19:22:24 -0400

    form submission handling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,17 @@
 // define variables
+var apiKey = "0b16f3c77b968f39ae09c648a502a860";
+var cityFormEl = document.querySelector("#city-form");
+var cityInputEl = document.querySelector("#cityName");
+var searchedCitiesEl = document.querySelector("#searched-cities");
+var todayEl = document.querySelector("#today");
+var forecastEl = document.querySelector("#forecast");
 
 // listen for user city search
+var submitForm = function (event) {
+    event.preventDefault();
+    var searchCityName = cityInputEl.value.trim();
+    console.log(cityInputEl.value);
+};
 
 // convert search to lat/long coordinates
 
@@ -11,3 +22,6 @@
 // display city 5 day forecast
 
 // save searched city to local storage and populate recent searches
+
+// event listeners
+cityFormEl.addEventListener("submit", submitForm);
