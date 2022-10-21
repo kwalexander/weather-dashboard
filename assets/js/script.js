deab98c6237c4df809e23c6f2ca5bac0881b1891
Kaspar Alexander <kaspar.alexander@gmail.com>
2022-10-20 20:14:04 -0400

    functions to search via open weather api

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,23 +5,88 @@ var cityInputEl = document.querySelector("#cityName");
 var searchedCitiesEl = document.querySelector("#searched-cities");
 var todayEl = document.querySelector("#today");
 var forecastEl = document.querySelector("#forecast");
+var cityName;
+var searchCityName;
+var lat = "";
+var lon = "";
+var geoCode = [];
+var cityArray = [];
 
-// listen for user city search
+// listen for user's city search from the form
 var submitForm = function (event) {
     event.preventDefault();
     var searchCityName = cityInputEl.value.trim();
-    console.log(cityInputEl.value);
+    console.log(searchCityName);
+    startSearch(searchCityName);
 };
 
-// convert search to lat/long coordinates
+// function to convert searched city value to lat/long coordinates
+var startSearch = function (searchCityName) {
 
-// query weather API with searched city
+    // get lat long geocode from open weather api
+    var getCityCoordinate = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCityName + "&appid=" + apiKey;
+
+    console.log(getCityCoordinate);
+
+    fetch(getCityCoordinate).then(function (response) {
+
+        if (response.ok) {
+            response.json().then(function (data) {
+                convertCoordinates(searchCityName, data);
+                //// store city in local storage array for history container
+                cityArray.push(searchCityName);
+                localStorage.setItem("searchedCities", JSON.stringify(cityArray));
+                searchHistory(cityArray);
+            });
+        } else { alert("Can't find city, search again") }
+    })
+};
+
+// display search history
+var searchHistory = function (cityArray) {
+    searchedCitiesEl.textContent = "";
+    for (var i = 0; i < cityArray.length; i++) {
+        let cityBtnEl = document.createElement('button')
+        searchedCitiesEl.appendChild(cityBtnEl);
+        cityBtnEl.textContent = cityArray[i];
+        cityBtnEl.setAttribute("value", cityArray[i]);
+    }
+};
+
+// convert to coordinates to feed into the weather request
+var convertCoordinates = function (searchCityName, data) {
+    lon = data.coord.lon;
+    lat = data.coord.lat;
+
+    geoCode.push(lat, lon);
+
+    console.log(geoCode);
+
+    requestWeather(cityName, geoCode);
+};
+
+// use the coordinates to get the weather 
+var requestWeather = function (cityName, coordinates) {
+    var currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+    fetch(currentWeatherURL).then(function (response) {
+        response.json().then(function (weatherData) {
+            // link to display current and forecast here
+            console.log(currentWeatherURL);
+        });
+    });
+
+    var forecastWeatherURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+    fetch(forecastWeatherURL).then(function (response) {
+        response.json().then(function (weatherData) {
+            // link to display current and forecast here
+            console.log(forecastWeatherURL);
+        });
+    });
+};
 
 // display city current weather
 
 // display city 5 day forecast
-
-// save searched city to local storage and populate recent searches
 
 // event listeners
 cityFormEl.addEventListener("submit", submitForm);
